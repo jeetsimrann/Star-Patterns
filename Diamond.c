a1417f3ed6d07670099bc0d3bc324dd6f4124e2a
Simran <sim1999@my.yorku.ca>
2020-11-09 11:43:21 +0530

    Added useless files to GitIgnore

---
+++ Diamond.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,r;
+    printf("Enter the number of rows:\n");
+    scanf("%d", &r);
+    for(i=0; i<(r+1)/2; i++){
+
+        for(j=(r+1)/2; j>i+1; j--){
+            printf(" ");
+        }
+
+        for(j=0; j<i*2+1; j++){
+                printf("*");
+        }
+        printf("\n");
+    }
+    for(i=0; i<(r-1)/2; i++){
+
+        for(j=0; j<i+1; j++){
+            printf(" ");
+        }
+
+        for(j=0; j<r-(i*2)-2; j++){
+                printf("*");
+        }
+        printf("\n");
+    }
+}
